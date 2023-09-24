f8d827272a2e91b00eadee56a0bfe4a7c5177891
jammisandy <109514629+jammisandy@users.noreply.github.com>
2023-09-23 19:58:32 -0500

    Update secgrp.tf

--- secgrp.tf
+++ secgrp.tf
@@ -53,7 +53,7 @@ resource "aws_security_group" "vprofile-prod-sg" {
      from_port = 22
      protocol = "tcp"
      to_port = 22
-     cidr_blocks = [aws_security_group.vprofile-bastion-sg.id]
+     cidr_blocks = ["0.0.0.0/0"]
    }
 }
 
